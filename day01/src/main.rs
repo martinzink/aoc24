aba1a0f66113d9d9c9fd0cd3d40c2d765e293f40
Martin Zink <martin.zink@apache.org>
2024-12-01 08:46:51 +0100

    day 01

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,38 @@
+use std::collections::HashMap;
+
+const EXAMPLE: &str = include_str!("example.txt");
+const INPUT: &str = include_str!("input.txt");
+
+fn parse_into_vecs(input: &str) -> (Vec<u32>, Vec<u32>) {
+    input
+        .lines()
+        .map(|line| {
+            let (a, b) = line.split_once(char::is_whitespace).unwrap();
+            let au32 = a.trim().parse::<u32>().unwrap();
+            let bu32 = b.trim().parse::<u32>().unwrap();
+            (au32, bu32)
+        }).unzip()
+}
+
+fn part_two(input: &str) -> u32 {
+    let (first, second): (Vec<u32>, Vec<u32>) = parse_into_vecs(input);
+    let counter = second.into_iter().fold(HashMap::new(), |mut acc: HashMap<u32, u32>, elt| {
+        *acc.entry(elt).or_insert(0) += 1;
+        acc
+    });
+    first.iter().fold(0, |a, &elt| a + counter.get(&elt).unwrap_or(&0) * elt)
+}
+
+fn part_one(input: &str) -> u32 {
+    let (mut first, mut second): (Vec<u32>, Vec<u32>) = parse_into_vecs(input);
+    first.sort();
+    second.sort();
+    first.iter().zip(second.iter()).fold(0, |acc, (a, b)| acc + a.abs_diff(*b))
+}
+
 fn main() {
-    println!("Hello 2024.12.01");
+    println!("Part One example: {}", part_one(EXAMPLE));
+    println!("Part One input: {}", part_one(INPUT));
+    println!("Part Two example: {}", part_two(EXAMPLE));
+    println!("Part Two input: {}", part_two(INPUT));
 }
