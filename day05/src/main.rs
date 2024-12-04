32faa09c467bac9989a373b08da8da110a5ede3f
Martin Zink <martin.zink@apache.org>
2024-12-04 14:09:06 +0100

    day 05 init

---
+++ day05/src/main.rs
@@ -0,0 +1,28 @@
+fn part_one(input: &str) -> i32 {
+    0
+}
+
+fn part_two(input: &str) -> i32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const EXAMPLE: &str = include_str!("example.txt");
+    #[test]
+    fn example_part_one() {
+        assert_eq!(part_one(EXAMPLE), 0);
+    }
+
+    #[test]
+    fn example_part_two() {
+        assert_eq!(part_two(EXAMPLE), 0);
+    }
+}
+
+fn main() {
+    const INPUT: &str = ""; // include_str!("input.txt");
+    println!("{} part one: {}", env!("CARGO_PKG_NAME"), part_one(INPUT));
+    println!("{} part two: {}", env!("CARGO_PKG_NAME"), part_two(INPUT));
+}
