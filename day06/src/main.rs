79f1b2330cb49340d23decd490683da234022392
Martin Zink <martin.zink@apache.org>
2024-12-06 08:44:56 +0100

    day 06 p2 brute force

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,9 +1,6 @@
-use std::cmp::Ordering;
-use std::collections::{HashMap, HashSet};
+use std::collections::{ HashSet};
 use std::error::Error;
 use std::ops::Add;
-use petgraph::graph::NodeIndex;
-use petgraph::visit::IntoNodeIdentifiers;
 
 #[derive(Debug, PartialEq, Clone, Copy, Eq, Hash)]
 struct Coord(i32, i32);
@@ -25,7 +22,7 @@ fn has_obstacle(coord: &Coord, matrix: &Vec<Vec<char>>) -> bool {
     matrix[coord.0 as usize][coord.1 as usize] == '#'
 }
 
-fn part_one(input: &str) -> i32 {
+fn count_distance(input: &str) -> HashSet<Coord> {
     let matrix = utils::parse_matrix(input);
     let mut guard_coord = Coord(0i32, 0i32);
     let mut max_i = 0;
@@ -55,7 +52,6 @@ fn part_one(input: &str) -> i32 {
             visited_coords.insert(next_guard_cord);
             guard_coord = next_guard_cord;
         } else {
-            println!("{:?}, {:?}", diff, guard_coord);
             diff = match diff {
                 Coord(-1, 0) => { Coord(0, 1) }
                 Coord(0, 1) => { Coord(1, 0) }
@@ -65,11 +61,71 @@ fn part_one(input: &str) -> i32 {
             }
         }
     }
-    visited_coords.len() as i32
+    visited_coords
+}
+
+fn part_one(input: &str) -> i32 {
+    count_distance(input).len() as i32
+}
+
+fn check_for_loop (max_i: i32, max_j: i32, matrix: &Vec<Vec<char>>, guard_init_coord: &Coord) -> bool {
+    let mut guard_coord = guard_init_coord.clone();
+    let mut visited_coords = HashSet::new();
+    let mut diff = Coord(-1, 0);
+    loop {
+        let next_guard_cord = &guard_coord + &diff;
+        if next_guard_cord.0 < 0 || next_guard_cord.1 < 0 {
+            return false;
+        }
+        if next_guard_cord.0 > max_i || next_guard_cord.1 > max_j {
+            return false
+        }
+
+        let next_diff = match diff {
+            Coord(-1, 0) => { Coord(0, 1) }
+            Coord(0, 1) => { Coord(1, 0) }
+            Coord(1, 0) => { Coord(0, -1) }
+            Coord(0, -1) => { Coord(-1, 0) }
+            Coord(_, _) => { panic!("Invalid direction")}
+        };
+
+        if !has_obstacle(&next_guard_cord, &matrix) {
+            if !visited_coords.insert((next_guard_cord, diff)) {
+                return true;
+            }
+            guard_coord = next_guard_cord;
+        } else {
+            diff = next_diff;
+        }
+    }
 }
 
 fn part_two(input: &str) -> i32 {
-    0
+    let matrix = utils::parse_matrix(input);
+    let mut guard_coord = Coord(0i32, 0i32);
+    let mut max_i = 0;
+    let mut max_j = 0;
+    let coords_to_check = count_distance(input);
+    for i in 0..matrix.len() {
+        for j in 0..matrix[i].len() {
+            if matrix[i][j] == '^' {
+                guard_coord = Coord(i as i32, j as i32);
+            }
+            max_j = std::cmp::max(max_j, j as i32);
+        }
+        max_i = std::cmp::max(max_i, i as i32);
+    }
+    let mut sum = 0;
+
+    for (i, coord_to_check) in coords_to_check.iter().enumerate() {
+        let mut clone_matrix = matrix.clone();
+        clone_matrix[coord_to_check.0 as usize][coord_to_check.1 as usize] = '#';
+        if check_for_loop(max_i, max_j, &clone_matrix, &guard_coord) {
+            sum += 1;
+        }
+    }
+
+    sum
 }
 
 #[cfg(test)]
@@ -83,12 +139,12 @@ mod tests {
 
     #[test]
     fn example_part_two() {
-        assert_eq!(part_two(EXAMPLE), 123);
+        assert_eq!(part_two(EXAMPLE), 6);
     }
 }
 
 fn main() {
     const INPUT: &str = include_str!("input.txt");
     println!("{} part one: {}", env!("CARGO_PKG_NAME"), part_one(INPUT));
-    //println!("{} part two: {}", env!("CARGO_PKG_NAME"), part_two(INPUT));
+    println!("{} part two: {}", env!("CARGO_PKG_NAME"), part_two(INPUT));
 }
