61ba5e905b6e7a972434bc2717975249ce215cab
Martin Zink <martinzink@apache.org>
2024-12-09 10:51:57 +0100

    day 09 p2

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -37,7 +37,7 @@ impl DiskMap {
         let mut i = 0u64;
         for block in &self.blocks {
             for _ in 0..block.size {
-                sum += (block.id.unwrap_or(0) as u64) * i as u64;
+                sum += (block.id.unwrap_or(0) as u64) * i;
                 i += 1;
             }
         }
@@ -92,41 +92,28 @@ fn part_one(input: &str) -> u64 {
 fn part_two(input: &str) -> u64 {
     let mut disk_map = DiskMap::new(input);
     let mut left_pointer = 0;
-    let mut right_pointer = disk_map.blocks.len() - 1;
     let mut new_blocks: Vec<BlockData> = Vec::new();
-    let mut right_most_free_space = 0;
-    while right_most_free_space < right_pointer {
-        if disk_map.blocks[left_pointer].id != None || disk_map.blocks[left_pointer].size == 0{
+    while left_pointer < disk_map.blocks.len() {
+        if disk_map.blocks[left_pointer].id != None || disk_map.blocks[left_pointer].size == 0 {
             new_blocks.push(disk_map.blocks[left_pointer].clone());
+            disk_map.blocks[left_pointer].id = None;
             left_pointer += 1;
             continue;
         }
-        if disk_map.blocks[right_pointer].id == None || disk_map.blocks[right_pointer].size == 0 {
-            right_pointer -= 1;
+        let free_space = disk_map.blocks[left_pointer].size;
+        let block_to_move = disk_map.blocks.iter_mut().rfind(|b| {
+            b.size <= free_space && b.id.is_some()
+        });
+        if block_to_move.is_none() {
+            new_blocks.push(BlockData {size:free_space, id: None});
+            left_pointer += 1;
             continue;
         }
-        let left_block_size = disk_map.blocks.get(left_pointer).unwrap().size;
-        let right_block_size = disk_map.blocks.get(right_pointer).unwrap().size;
-        match left_block_size.cmp(&right_block_size) {
-            Ordering::Equal => {
-                new_blocks.push(disk_map.blocks.get(right_pointer).unwrap().clone());
-                left_pointer += 1;
-                right_pointer -= 1;
-            }
-            Ordering::Less => {
-                let right_block = disk_map.blocks.get_mut(right_pointer).unwrap();
-                let splits = right_block.split(right_block_size-left_block_size);
-                new_blocks.push(splits[1]);
-                right_block.size = splits[0].size;
-                left_pointer += 1;
-            }
-            Ordering::Greater => {
-                new_blocks.push(disk_map.blocks.get(right_pointer).unwrap().clone());
-                let left_block = disk_map.blocks.get_mut(left_pointer).unwrap();
-                left_block.size = left_block_size - right_block_size;
-                right_pointer -= 1;
-            }
-        }
+        let block_to_move = block_to_move.unwrap();
+        let block_size = block_to_move.size;
+        new_blocks.push(block_to_move.clone());
+        block_to_move.id = None;
+        disk_map.blocks[left_pointer].size = free_space - block_size;
     }
 
     let new_map = DiskMap{blocks: new_blocks};
@@ -147,7 +134,7 @@ mod tests {
 
     #[test]
     fn example_part_two() {
-        assert_eq!(part_two(EXAMPLE), 0);
+        assert_eq!(part_two(EXAMPLE), 2858);
     }
 }
 
